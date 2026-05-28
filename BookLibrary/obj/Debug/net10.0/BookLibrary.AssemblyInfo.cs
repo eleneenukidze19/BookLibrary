ede0b901580a93601c152175c56ba658e61034d9
eleneenukidze19 <eleneenukidze2020@gmail.com>
2026-05-28 16:34:53 +0400

    first commit

--- BookLibrary/obj/Debug/net10.0/BookLibrary.AssemblyInfo.cs
+++ BookLibrary/obj/Debug/net10.0/BookLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a342acd6934256ef7966fb53e7c31783d9794f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
