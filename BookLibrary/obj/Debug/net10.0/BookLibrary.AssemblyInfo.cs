64318a29c65127409ac6d29885d287325bb7da28
eleneenukidze19 <eleneenukidze2020@gmail.com>
2026-06-12 16:52:31 +0400

    Added frontend design

--- BookLibrary/obj/Debug/net10.0/BookLibrary.AssemblyInfo.cs
+++ BookLibrary/obj/Debug/net10.0/BookLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede0b901580a93601c152175c56ba658e61034d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0881bd98c0706f6fee3abd56926ecb3e2996f042")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
